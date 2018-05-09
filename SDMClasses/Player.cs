7209632b1209bb76422b4412092cfd930774b48d
Pedro9558 <pedro.albanez@gmail.com>
2018-05-08 22:16:32 -0300

    Removi variaveis desnecessarias

--- SDMClasses/Player.cs
+++ SDMClasses/Player.cs
@@ -1,14 +1,10 @@
-﻿using System;
-namespace SDMClasses
+﻿namespace SDMClasses
 {
-
     public class Player
     {
         private string _nome;
         // Versão atualizada da pontuação
         private double _pontuacao;
-        // Versão anterior da pontuação
-        private double _pontuacaoAnterior;
         public string Nome
         {
             get { return this._nome; }
@@ -19,22 +15,15 @@ namespace SDMClasses
             get { return this._pontuacao; }
             set { this._pontuacao = value; }
         }
-        public double PontuacaoAnterior
-        {
-            get { return this._pontuacaoAnterior; }
-            set { this._pontuacaoAnterior = value; }
-        }
         public Player(string Nome)
         {
             this.Nome = Nome;
             Pontuacao = 0.0;
-            PontuacaoAnterior = 0.0;
         }
         public Player(string Nome, double PontuacaoAnterior)
         {
             this.Nome = Nome;
             Pontuacao = 0.0;
-            this.PontuacaoAnterior = PontuacaoAnterior;
         }
     }
 }
